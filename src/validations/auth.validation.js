afd57e7a9e00234dfe9941974aeea7debc140cc6
deepaksonictechpro <deepak.sonictechpro@gmail.com>
2026-03-09 19:26:45 +0530

    some comment changes

--- src/validations/auth.validation.js
+++ src/validations/auth.validation.js
@@ -1,6 +1,7 @@
 const Joi = require("joi");
 
-// ================= REGISTER =================
+//------------------------------- REGISTER -------------------------------------------
+
 const registerSchema = Joi.object({
   fullName: Joi.string().min(3).required(),
   email: Joi.string().email().required(),
@@ -10,37 +11,41 @@ const registerSchema = Joi.object({
     .required(),
 });
 
-// ================= VERIFY REGISTER OTP =================
+//------------------------------- VERIFY REGISTER OTP --------------------------------
 const verifyOtpSchema = Joi.object({
   email: Joi.string().email().required(),
   otp: Joi.string().length(6).required(),
 });
 
-// ================= LOGIN =================
+//------------------------------- LOGIN ----------------------------------------------
 const loginSchema = Joi.object({
   email: Joi.string().email().required(),
   password: Joi.string().required(),
   role: Joi.string().required(),
 });
 
-// ================= FORGOT PASSWORD =================
+//------------------------------- FORGOT PASSWORD ------------------------------------
+
 const forgotPasswordSchema = Joi.object({
   email: Joi.string().email().required(),
 });
 
-// ================= VERIFY FORGOT OTP =================
+//------------------------------- VERIFY FORGOT OTP ----------------------------------
+
 const verifyForgotOtpSchema = Joi.object({
   email: Joi.string().email().required(),
   otp: Joi.string().length(6).required(),
 });
 
-// ================= RESET PASSWORD =================
+//------------------------------- RESET PASSWORD -------------------------------------
+
 const resetPasswordSchema = Joi.object({
   email: Joi.string().email().required(),
   newPassword: Joi.string().min(6).required(),
 });
 
-// ================= CREATE ADMIN =================
+//------------------------------- CREATE ADMIN ---------------------------------------
+
 const createAdminSchema = Joi.object({
   fullName: Joi.string().min(3).required(),
   email: Joi.string().email().required(),
